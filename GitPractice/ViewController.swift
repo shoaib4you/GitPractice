00eb8f840bf9031d19c51acfebbcf3d92396050e
Shoaib <shoaibkhan133@gmail.com>
2023-08-17 15:35:53 +0530

    Initial Commit

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by mac on 17/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    func login() {
+        print("This is login")
+    }
+
+}
+
